da8f5813cbc1f830a45425d4b6d0fee0697f8154
RAKESH JADHAV <95296856+Rakesh1322@users.noreply.github.com>
2022-12-13 15:28:04 +0530

    Update 8-puzzle(ids).py

--- 8-puzzle(ids).py
+++ 8-puzzle(ids).py
@@ -1,103 +1,58 @@
-import copy
+src = [1, 2, 5, 3,4, -1, 6, 7, 8]
+target = [-1, 1, 2, 3, 4, 5, 6, 7, 8]
 
-inp=[[1,2,5],[3,4,-1],[6,7,8]]
-out=[[-1,1,2],[3,5,4],[6,7,8]]
+def idfs(src,target,depth):
+  for limit in range(0,depth+1):
+    vis=[]
+    if dfs(src,target,limit,vis):
+      return True
+  return False
+
+def gen(state,m,b):
+  temp=state[:]
+  if m=='l':
+    temp[b],temp[b-1]=temp[b-1],temp[b]
+  if m=='r':
+    temp[b],temp[b+1]=temp[b+1],temp[b]
+  if m=='u':
+    temp[b],temp[b-3]=temp[b-3],temp[b]
+  if m=='d':
+    temp[b],temp[b+3]=temp[b+3],temp[b]
+
+  return temp
+
+def possible_move(state,vis):
+  b=state.index(-1)
+  dir=[]
+  move=[]
+  if b<=5:
+    dir.append('d')
+  if b>=3:
+    dir.append('u')
+  if b%3>0:
+    dir.append('l')
+  if b%3<2:
+    dir.append('r')
+  
+  for i in dir:
+    temp=gen(state,i,b)
+    if not temp in vis:
+      move.append(temp)
+
+  print(move)
+  return move
+
+def dfs(src,target,limit,vis):
+  if src==target :
+    return True
+  if limit<0 :
+    return False
+  vis.append(src)
+  move=possible_move(src,vis)
+  for m in move :
+    if dfs(m,target,limit-1,vis):
+      return True
+  return False
 
 
-def move(temp, movement):
-  if movement=="up":
-    for i in range(3):
-      for j in range(3):
-        if(temp[i][j]==-1):
-          # blank space found
-          if i!=0:
-            temp[i][j]=temp[i-1][j]
-            temp[i-1][j]=-1
-          return temp
-
-  if movement=="down":
-    for i in range(3):
-      for j in range(3):
-        if(temp[i][j]==-1):
-          # blank space found
-          if i!=2:
-            temp[i][j]=temp[i+1][j]
-            temp[i+1][j]=-1
-          return temp
-
-  if movement=="left":
-    for i in range(3):
-      for j in range(3):
-        if(temp[i][j]==-1):
-          # blank space found
-          if j!=0:
-            temp[i][j]=temp[i][j-1]
-            temp[i][j-1]=-1
-          return temp
-
-  if movement=="right":
-    for i in range(3):
-      for j in range(3):
-        if(temp[i][j]==-1):
-          # blank space found
-          if j!=2:
-            temp[i][j]=temp[i][j+1]
-            temp[i][j+1]=-1
-          return temp
-
-def ids():
-  global inp
-  global out
-  global flag
-
-  for limit in range(100):
-    print('LIMIT -> '+str(limit))
-    stack=[]
-    inpx=[inp,"none"]
-    stack.append(inpx)
-    # for i in range(9):
-    level=0
-    while(True):
-      if len(stack)==0:
-        break
-      puzzle=stack.pop(0)
-      if level<=limit:
-        print(str(puzzle[1])+" --> "+str(puzzle[0]))
-        if(puzzle[0]==out):
-          print("Found")
-          print('Path cost='+str(level))
-          flag=True
-          return
-        else:
-          level=level+1
-          # up
-          if(puzzle[1]!="down"):
-            temp=copy.deepcopy(puzzle[0])
-            up=move(temp, "up")
-            if(up!=puzzle[0]):
-              upx=[up,"up"]
-              stack.insert(0, upx)
-          # left
-          if(puzzle[1]!="right"):
-            temp=copy.deepcopy(puzzle[0])
-            left=move(temp, "left")
-            if(left!=puzzle[0]):
-              leftx=[left,"left"]
-              stack.insert(0, leftx)
-          # down
-          if(puzzle[1]!="up"):
-            temp=copy.deepcopy(puzzle[0])
-            down=move(temp, "down")
-            if(down!=puzzle[0]):
-              downx=[down,"down"]
-              stack.insert(0, downx)
-          # right
-          if(puzzle[1]!="left"):
-            temp=copy.deepcopy(puzzle[0])
-            right=move(temp, "right")
-            if(right!=puzzle[0]):
-              rightx=[right,"right"]
-              stack.insert(0, rightx)
-          
-
-ids()
+print(idfs(src,target,5))
